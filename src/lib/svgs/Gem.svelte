f01097aa006cf30146afa73a45ca9759679679d0
Fastre <61952761+IamFastre@users.noreply.github.com>
2025-11-15 05:59:45 +0200

    svgs

--- src/lib/svgs/Gem.svelte
+++ src/lib/svgs/Gem.svelte
@@ -1,4 +1,4 @@
-<svg viewBox="0 0 50 50">
+<svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
   <mask id="gem-mask">
     <rect
       id="gem-shape"
--- src/lib/svgs/index.ts
+++ src/lib/svgs/index.ts
@@ -1,2 +1,5 @@
-export { default as Gem } from "./Gem.svelte";
-export { default as SineWave } from "./SineWave.svelte";
+export { default as Discord   } from "./Discord.svelte";
+export { default as Gem       } from "./Gem.svelte";
+export { default as Instagram } from "./Instagram.svelte";
+export { default as SineWave  } from "./SineWave.svelte";
+export { default as Threads   } from "./Threads.svelte";
